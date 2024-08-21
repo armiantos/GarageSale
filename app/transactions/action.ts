981df820ac817191b222fd28a2a5bd4bb3c5c7d0
Armianto Sumitro <armiantos@gmail.com>
2024-08-21 00:03:20 -0600

    Add item filtering in transaction

--- app/transactions/action.ts
+++ app/transactions/action.ts
@@ -2,11 +2,29 @@
 
 import { DEFAULT_PAGE_SIZE } from "@/config";
 import prisma from "@/db";
+import { Prisma } from "@prisma/client";
 
 export async function getTransactions(page: number, search?: string) {
-  const totalRecordCount = await prisma.item.count();
+  const whereClause: Prisma.TransactionWhereInput =
+    search !== undefined
+      ? {
+          TransactionItem: {
+            some: {
+              item: {
+                id: {
+                  contains: search,
+                  mode: "insensitive",
+                },
+              },
+            },
+          },
+        }
+      : {};
+
+  const totalRecordCount = await prisma.transaction.count({ where: whereClause });
 
   const transactions = await prisma.transaction.findMany({
+    where: whereClause,
     skip: page * DEFAULT_PAGE_SIZE,
     take: DEFAULT_PAGE_SIZE,
     include: {
